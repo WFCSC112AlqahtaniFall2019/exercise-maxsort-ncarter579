708ca92e5d3ec981aff48c6d2bcf29b2baff9488
Nathan Carter <cartnl18@wfu.edu>
2019-09-11 21:58:48 -0400

    Finished code

---
+++ cmake-build-debug/CMakeFiles/MaxSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaxSort.dir/maxSort.cpp.obj"
+  "MaxSort.pdb"
+  "MaxSort.exe"
+  "MaxSort.exe.manifest"
+  "libMaxSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MaxSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- maxSort.cpp
+++ maxSort.cpp
@@ -12,18 +12,22 @@ int main() {
     vector<int> v(5);  // vector to be sorted
     // initialize v
     /* your code here */
-
+    v = {6,1,3,2,1};
 
     // print the unsorted v
     /* your code here */
-
+    print(v);
 
     // sort v
     /* your code here */
+    maxSort(v);
+
+
 
 
     // print the sorted v
     /* your code here */
+    print(v);
 
 
     return 0;
@@ -35,22 +39,37 @@ int main() {
 void maxSort(vector<int>& list){
     for(int i=list.size()-1;i>=0;i--){
         /*complete the code*/
-
-        for(int j=0;j<=i;j++){
-            /*complete the code*/
+        int current_max = 0;
+        /*complete the code*/
+        for(int j = 0; j <= i; j++){
+            if(list[j]>list[current_max]){
+                current_max = j;
+            }
+            swap(list[current_max], list[i]);
 
         }
-        /*complete the code*/
+
     }
+    return;
 }
 
 // swap function
 void swap(int& a, int&b){
     /*your code here*/
+    int temp = 0;
+    temp = a;
+    a = b;
+    b = temp;
+
 }
 
 // print function
 void print(const vector<int>& list){
     /*your code here*/
+    for(int i = 0; i < 5; i++){
+        cout << list.at(i);
+    }
+    cout << endl;
+    return;
 }
 
